803f8feb56fe586f25b146f869cc239985165ca2
Yasmeen Waleed <yw20282@gmail.com>
2025-10-30 21:54:47 +0300

    init hive and open notes box

--- notes_app/lib/const_variable/constants.dart
+++ notes_app/lib/const_variable/constants.dart
@@ -1,3 +1,4 @@
 
 import 'package:flutter/cupertino.dart';
-const KPrimaryColor=const Color(0xff62FCD7);
+const KPrimaryColor= Color(0xff62FCD7);
+const KNotesBox='notes_box';
--- notes_app/lib/main.dart
+++ notes_app/lib/main.dart
@@ -1,7 +1,12 @@
 
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/const_variable/constants.dart';
 import 'package:notes_app/views/notes_views.dart';
-void main() {
+void main()async {
+  await Hive.initFlutter();
+
+  await Hive.openBox(KNotesBox);
   runApp(const NotesApp ());
 }
 
