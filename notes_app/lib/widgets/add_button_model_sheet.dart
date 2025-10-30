91389ca4918f68d7c583d1b9f7b445891c670ebe
Yasmeen Waleed <yw20282@gmail.com>
2025-10-30 17:58:40 +0300

    add field to button sheet

--- notes_app/lib/widgets/add_button_model_sheet.dart
+++ notes_app/lib/widgets/add_button_model_sheet.dart
@@ -11,7 +11,14 @@ class AddButtonModelSheet extends StatelessWidget {
       child: Column(
       children: [
         SizedBox(height: 32,),
-        CustomTextField(),
+        CustomTextField(
+          hint: 'title',
+        ),
+        SizedBox(height: 16,),
+         CustomTextField(
+          hint: 'Content',
+          maxLines: 5,
+        ),
       ],
       ),
     );
--- notes_app/lib/widgets/custom_text_field.dart
+++ notes_app/lib/widgets/custom_text_field.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/const_variable/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField({super.key, required this.hint,  this.maxLines=1});
+final String hint;
+final int maxLines;
   @override
   Widget build(BuildContext context) {
     return  TextField(
       cursorColor:KPrimaryColor ,
+      maxLines:maxLines ,
       decoration: InputDecoration(
-        hintText: 'Title',
+        hintText:hint,
         border: buildBorder(),
         enabledBorder: buildBorder(),
         focusedBorder: buildBorder(KPrimaryColor),
