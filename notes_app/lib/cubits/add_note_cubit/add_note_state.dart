94930d0a40c05e2ad022ccd80d4d45ac5c8c87d5
Yasmeen Waleed <yw20282@gmail.com>
2025-10-30 23:25:44 +0200

    add note state

---
+++ notes_app/lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,17 @@
+part of 'add_note_cubit.dart';
+
+@immutable
+
+abstract class AddNoteState {}
+class AddNoteInitial extends AddNoteState{}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteFailure extends AddNoteState {
+  final String errMessage;
+  AddNoteFailure({required this.errMessage});
+}
+  
+  
