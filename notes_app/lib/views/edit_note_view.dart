a59ac63a6a654b6fa6711a8ef78fbd861716d66e
Yasmeen Waleed <yw20282@gmail.com>
2025-10-30 18:32:23 +0300

    create edit note view

---
+++ notes_app/lib/views/edit_note_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/edit_note_view_body.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:EditNoteViewBody() ,
+    );
+  }
+}
+
--- notes_app/lib/widgets/custom_note_item.dart
+++ notes_app/lib/widgets/custom_note_item.dart
@@ -1,40 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
-      decoration: BoxDecoration(
-        color: Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text('Flutter Tips',
-            style: TextStyle(color: Colors.black,fontSize: 26),),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16,bottom: 16),
-              child: Text('Build your career with Yasmeen Waleed',
-              style: TextStyle(color: Colors.black,fontSize: 18),),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context)
+        {
+          return EditNoteView();
+        }
+        ));
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
+        decoration: BoxDecoration(
+          color: Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+      
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text('Flutter Tips',
+              style: TextStyle(color: Colors.black,fontSize: 26),),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16,bottom: 16),
+                child: Text('Build your career with Yasmeen Waleed',
+                style: TextStyle(color: Colors.black,fontSize: 18),),
+              ),
+              trailing: IconButton(
+                onPressed: (){},
+               icon: Icon(Icons.delete,
+               color: Colors.black,
+               size: 24,),),
             ),
-            trailing: IconButton(
-              onPressed: (){},
-             icon: Icon(Icons.delete,
-             color: Colors.black,
-             size: 24,),),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text('oct28,2000',style: TextStyle(color: Colors.black,fontSize: 16),),
-          ),
-        ],
+      
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text('oct28,2000',style: TextStyle(color: Colors.black,fontSize: 16),),
+            ),
+          ],
+        ),
       ),
     );
   }
