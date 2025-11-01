e899f2eed0380678cbad213d1798836cd21da992
Yasmeen Waleed <yw20282@gmail.com>
2025-11-01 04:45:13 +0200

    add add note cubit to add note botton sheet

--- notes_app/lib/widgets/add_button_model_sheet.dart
+++ notes_app/lib/widgets/add_button_model_sheet.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/custom_button.dart';
-import 'package:notes_app/widgets/custom_text_field.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/widgets/add_note_form.dart';
 
 class AddButtonModelSheet extends StatelessWidget {
   const AddButtonModelSheet({super.key});
@@ -8,71 +10,25 @@ class AddButtonModelSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      padding:  EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: AddNoteForm(),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if(state is AddNoteFailure){
+              print('failied ${state.errMessage}');
+
+            }
+            if(state is AddNoteSuccess){
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading?true:false,
+              child: AddNoteForm());
+          },
+        ),
       ),
     );
   }
 }
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState>formKey=GlobalKey();
-  AutovalidateMode autoValidateMode=AutovalidateMode.disabled;
-  String? title,subTitle;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autoValidateMode,
-      child: Column(
-      children: [
-        SizedBox(height: 32,),
-        CustomTextField(
-          onSaved: (value) {
-            title=value;
-            
-          },
-          hint: 'title',
-        ),
-        SizedBox(height: 16,),
-         CustomTextField(
-          onSaved: (value){
-            subTitle=value;
-          },
-          hint: 'Content',
-          maxLines: 5,
-        ),
-      
-        SizedBox(height: 32,),
-      
-        CustomButton(
-          onTap: () {
-            if(formKey.currentState!.validate()){
-                       formKey.currentState!.save();
-            }
-            else{
-              autoValidateMode=AutovalidateMode.always;
-            }
-            setState(() {
-              
-            });
-          },
-        ),
-        SizedBox(height: 16,),
-      ],
-      ),
-    );
-  }
-}
-
-
